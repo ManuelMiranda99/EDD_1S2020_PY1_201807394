cad5feb6c94c6a8620979b6edcf19fb88e4c6b40
Manuel Miranda <3057476220301@ingenieria.usac.edu.gt>
2020-03-08 18:04:17 -0600

    Lista doblemente enlazada fichas
    Lista doblemente enlazada usada para guardar las fichas del jugador mientras juega. Implemetado metodos de Usuarios

--- User.cpp
+++ User.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "User.h"
+
+User::User(string _name) {
+    name = _name;
+    coins = new DoubleList();
+    scores = new SimpleListIS(name);
+    left = right = NULL;
+}
+
+void User::GetPoints(int _points) {
+    points += _points;
+}
+
+void User::StartGame() {
+    points = 0;
+}
+
+int User::GetMaximumScore() {
+    return scores->first->score;
+}
+
+void User::GenerateScoresReport() {
+    scores->GenerateReport();
+}
+
+void User::GenerateCoinsReport() {
+    coins->GenerateReport();
+}
+
+void User::AddCoins(char _letter) {
+    coins->InsertNode(_letter);
+}
+
+void User::AddScore() {
+    scores->InsertScore(points);
+}
--- User.h
+++ User.h
@@ -9,42 +9,47 @@
 
 using namespace std;
 
-class SimpleListIS{
-    class NodeIS{
-        public:
-            int score;
-            NodeIS *next;
-            NodeIS(int _score){
-                score = _score;
-                next = NULL;
-            }
+class SimpleListIS {
+    class NodeIS {
+    public:
+        int score;
+        NodeIS *next;
+
+        NodeIS(int _score) {
+            score = _score;
+            next = NULL;
+        }
     };
+
+public:
+
     NodeIS *first;
     int size;
     string user;
-    SimpleListIS(string _user){
+
+    SimpleListIS(string _user) {
         first = NULL;
         user = _user;
         size = 0;
     }
 
-    void InsertScore(int _score){
+    void InsertScore(int _score) {
         NodeIS *newNode = new NodeIS(_score);
 
-        if(first == NULL){
+        if (first == NULL) {
             first = newNode;
-        }else{
+        } else {
             NodeIS *aux1 = first;
             NodeIS *aux2;
 
-            while((aux1 != NULL) && (aux1->score > _score)){
+            while ((aux1 != NULL) && (aux1->score > _score)) {
                 aux2 = aux1;
                 aux1 = aux1->next;
             }
 
-            if(aux1 == first){
+            if (aux1 == first) {
                 first = newNode;
-            }else{
+            } else {
                 aux2->next = newNode;
             }
 
@@ -52,53 +57,217 @@ class SimpleListIS{
         }
         size++;
     }
-    void GenerateReport(){
+
+    void GenerateReport() {
         string graph = "digraph L { \n"
                        "rankdir=LR;";
         int x = 1;
         NodeIS *aux = this->first;
-        if(aux != NULL){
-            do{
-                graph += "X" + to_string(x) + " [shape=box,color=lightblue,style=filled,label=\"" + to_string(aux->score) + "\"];\n";
+        if (aux != NULL) {
+            do {
+                graph += "X" + to_string(x) + " [shape=box,color=lightblue,style=filled,label=\"" +
+                         to_string(aux->score) + "\"];\n";
                 x++;
                 aux = aux->next;
-            }while (aux != NULL);
+            } while (aux != NULL);
             x = 1;
-            do{
-                if(x == size){
+            do {
+                if (x == size) {
                     graph += "X" + to_string(x);
-                }else{
+                } else {
                     graph += "X" + to_string(x) + " -> ";
                     x++;
                     aux = aux->next;
                 }
-            }while(aux != NULL);
+            } while (aux != NULL);
             graph += "; \n }";
 
             ofstream writeToFile;
             writeToFile.open("ScoresByPlayer.txt", ios_base::out | ios_base::trunc);
-            if(writeToFile.is_open()){
+            if (writeToFile.is_open()) {
                 writeToFile << graph;
                 writeToFile.close();
             }
 
             system("dot -Tpng ScoresByPlayer.txt -o C:/Users/Manuel/Desktop/ScoresByPlayer.png");
             system("C:/Users/Manuel/Desktop/ScoresByPlayer.png");
+        }
+    };
+
+};
+
+class DoubleList {
+    class NodeIC {
+    public:
+        char letter;
+        int points;
+        NodeIC *previous, *next;
+
+        NodeIC(char _letter) {
+            letter = _letter;
+
+            switch (letter) {
+                case 'A':
+                case 'E':
+                case 'O':
+                case 'I':
+                case 'S':
+                case 'N':
+                case 'L':
+                case 'R':
+                case 'U':
+                case 'T':
+                    points = 1;
+                    break;
+                case 'D':
+                case 'G':
+                    points = 2;
+                    break;
+                case 'C':
+                case 'B':
+                case 'M':
+                case 'P':
+                    points = 3;
+                    break;
+                case 'H':
+                case 'F':
+                case 'V':
+                case 'Y':
+                    points = 4;
+                    break;
+                case 'Q':
+                    points = 5;
+                    break;
+                case 'J':
+                case ';':
+                case -92:
+                case 'X':
+                    points = 8;
+                    break;
+                case 'Z':
+                    points = 10;
+                    break;
+                default:
+                    points = 0;
+                    break;
+            }
+
+            previous = NULL;
+            next = NULL;
+        }
+    };
+
+private:
+    NodeIC *first, *last;
+    int size;
+public:
+    DoubleList() {
+        first = last = NULL;
+        size = 0;
     }
+
+    void InsertNode(char _letter) {
+        NodeIC *newNode = new NodeIC(_letter);
+
+        if (first == NULL) {
+                first = last= newNode;
+        }else{
+            last->next = newNode;
+            newNode->previous = last;;
+
+            last = newNode;
+        }
+        size++;
+    }
+
+    void DeleteNode(char _letter) {
+        if(first != NULL){
+            NodeIC *aux2, *aux = first;
+            while(aux != NULL){
+                if(aux->letter == _letter){
+                    break;
+                }else{
+                    aux2 = aux;
+                    aux = aux->next;
+                }
+            }
+            if(aux != NULL){
+                if(aux->next != NULL){
+                    aux2->next = aux->next;
+                    aux->next->previous = aux2;
+                }else{
+                    aux2->next = NULL;
+                }
+            }
+        }
+    }
+
+    string GetCoins() {
+        string text = "";
+        if(first != NULL){
+            NodeIC *aux = first;
+            while(aux != last){
+                text += aux->letter + ", ";
+                aux = aux->next;
+            }
+            text += aux->letter;
+        }
+        return text;
+    }
+
+    void GenerateReport() {
+        string graph = "digraph L {\n";
+        int x = 1;
+        NodeIC *aux = first;
+        if (aux != NULL) {
+            while (aux != NULL) {
+                graph += "X" + to_string(x) + " [color=lightgray,style=filled, label=\"" + aux->letter + "\"];\n";
+                x++;
+                aux = aux->next;
+            }
+            graph += "nullNode [shape=none,label=\"null\"];\n";
+            x = 1;
+            aux = first;
+            while (aux != NULL) {
+                graph += "X" + to_string(x) + " -> ";
+                x++;
+                aux = aux->next;
+            }
+            x--;
+            graph += " nullNode; \n";
+            while (x > 1) {
+                graph += "X" + to_string(x) + "->";
+                x--;
+            }
+            graph += " X1; \n }";
+            ofstream writeToFile;
+            writeToFile.open("FichasIndividuales.txt", ios_base::out | ios_base::trunc);
+            if (writeToFile.is_open()) {
+                writeToFile << graph;
+                writeToFile.close();
+            }
+
+            system("dot -Tpng FichasIndividuales.txt -o C:/Users/Manuel/Desktop/FichasIndividuales.png");
+            system("C:/Users/Manuel/Desktop/FichasIndividuales.png");
+        }
+
+    };
 };
 
 class User {
     public:
         string name;
         User *left, *right;
-        /*
-         * SimpleListIS scores; Add to the constructor
-         * DoubleList coins; Add to the constructor
-         */
+
+        SimpleListIS *scores;
+        DoubleList *coins;
+
         int points;
         User(string);
         void GetPoints(int);
         void StartGame();
+        void AddCoins(char);
+        void AddScore();
         int GetMaximumScore();
         void GenerateScoresReport();
         void GenerateCoinsReport();
